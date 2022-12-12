f73164bb453fe4188414ad91fb98ab03c69a411d
Daniel Jasinski <102223345+djasinskis@users.noreply.github.com>
2022-12-12 17:34:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const card = document.querySelectorAll(".card")
+
+for(let i = 0; i < card.length; i++){
+  card[i].addEventListener( 'click', function() {
+  card[i].classList.toggle('flipCard');
+});
+}
+
